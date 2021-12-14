783ab8f03a1db44562c6878ab33822687b005a85
AARON--789 <95288848+aaron-6734745663675765765657@users.noreply.github.com>
2021-12-14 13:26:27 +0100

    hhiujuoo

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
 let LUZ = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(input.temperature())
+})
 input.onSound(DetectedSound.Loud, function () {
     if (LUZ == 0) {
         LUZ = 0
@@ -21,6 +24,20 @@ input.onSound(DetectedSound.Loud, function () {
         . . # # .
         `)
     pins.digitalWritePin(DigitalPin.P0, 0)
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 3; index++) {
+        basic.showIcon(IconNames.SmallHeart)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(100)
+    }
 })
 basic.forever(function () {
 	
